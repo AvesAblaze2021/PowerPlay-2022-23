a7b9b48c1973f2255b10683152a0d520f9b735d5
AvesAblaze2021 <90651389+AvesAblaze2021@users.noreply.github.com>
2022-09-26 16:07:18 -0400

    Create DriveTest.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ablaze/test/DriveTest.java
@@ -0,0 +1,16 @@
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+import org.firstinspires.ftc.teamcode.FreightFrenzy202122.ablaze.common.AblazeRobot;
+import org.firstinspires.ftc.teamcode.FreightFrenzy202122.ablaze.common.AblazeRobotTest;
+
+
+@Autonomous
+public class MotorTest extends LinearOpMode {
+
+    @Override
+    public void runOpMode(){
+      
+    }
+}
