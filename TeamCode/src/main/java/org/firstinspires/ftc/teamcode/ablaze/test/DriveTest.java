0db7d72c73d3a0604616026f5c7e5129cd9d063b
AvesAblaze2021 <90651389+AvesAblaze2021@users.noreply.github.com>
2022-10-10 16:35:14 -0400

    basic setup/init for drivetest

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ablaze/test/DriveTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ablaze/test/DriveTest.java
@@ -2,15 +2,30 @@ package org.firstinspires.ftc.teamcode.ablaze.test;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
 
+import org.firstinspires.ftc.teamcode.FreightFrenzy202122.ablaze.common.PracticeRobot;
 import org.firstinspires.ftc.teamcode.ablaze.common.*;
 
-
+//Runs each motor once
 @Autonomous
 public class DriveTest extends LinearOpMode {
+    PracticeRobot robot;
 
     @Override
     public void runOpMode(){
-      
+        robot = new PracticeRobot();
+        robot.initialize(hardwareMap);
+
+        //LF Motor
+
+
+        //RF Motor
+
+        //RB Motor
+
+        //LB Motor
+
+        
     }
 }
