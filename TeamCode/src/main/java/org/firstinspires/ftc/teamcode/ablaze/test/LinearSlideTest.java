4a212b5f55147027e71594b0a900ed71a8e8f8af
AvesAblaze2021 <90651389+AvesAblaze2021@users.noreply.github.com>
2022-12-01 11:41:35 -0500

    implemented encoder/time algorithms for linear slides

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ablaze/test/LinearSlideTest.java
@@ -0,0 +1,58 @@
+package org.firstinspires.ftc.teamcode.ablaze.autonomous;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import org.firstinspires.ftc.teamcode.ablaze.common.AblazeRobot;
+import java.util.Math;
+
+@Autonomous
+public class LinearSlideTest extends LinearOpMode{
+  AblazeRobot robot = new AblazeRobot();
+  int lastPos = 0; //Used for encoder algorithm
+  double motorPower = 0.2;
+  
+  @Override
+  public void runOpMode(){
+    robot.initalize(hardwareMap);
+    DcMotor horizontal = robot.getHorizontalSlideMotor();
+    DcMotor vertical = robot.getVerticalSlideMotor();
+    moveSlideEncoder(vertical, 200, 500); //timeout in MS
+  }
+  
+  //Regular timed algorithm
+  public void moveSlideTime(DcMotor slideMotor, double power, int time){
+    slideMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+    slideMotor.setPower(power);
+    sleep(time);
+    slideMotor.setPower(0);
+  }
+  
+  //Option 2 algorithm described in pseudocode - see drive
+  public void moveSlideEncoder(DcMotor slideMotor, int ticks, int timeout){
+    //Encoder setup - adjust tick value w/ last pos
+    int newTicks = Math.abs(ticks - lastPos);
+    slideMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+    slideMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+    slideMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+    
+    //Starting motor
+    slideMotor.setTargetPosition(newTicks);
+    slideMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+    slideMotor.setPower(0.2);
+    
+    //Encoder loop
+    runtime.reset()
+    while(opModeIsActive() && runtime.milliseconds() < timeout && slideMotor.isBusy()){
+      continue;
+    }
+    
+    //End motion + store current tick value before reset
+    slideMotor.setPower(0);
+    lastPos = slideMotor.getCurrentPosition();
+    slideMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+  }
+    
+}
